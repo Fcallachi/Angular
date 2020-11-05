18528f115e04ac7dd5ed2ed022fb48ebd9299b59
unknown <fernandocallachi@gmail.com>
2020-11-04 22:29:24 -0300

    integrando a tela de login e cadastro ao sistema

--- Blog_Pessoal/blogPessoal/src/app/app.module.ts
+++ Blog_Pessoal/blogPessoal/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,6 +10,10 @@ import { FooterComponent } from './footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { FeedComponent } from './feed/feed.component';
 import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { from } from 'rxjs';
+import { TermoComponent } from './termo/termo.component';
 
 @NgModule({
   declarations: [
@@ -17,12 +22,16 @@ import { PerfilLateralComponent } from './perfil-lateral/perfil-lateral.componen
     NavbarComponent,
     FooterComponent,
     FeedComponent,
-    PerfilLateralComponent
+    PerfilLateralComponent,
+    LoginComponent,
+    CadastroComponent,
+    TermoComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ Blog_Pessoal/blogPessoal/src/app/model/User.ts
@@ -0,0 +1,6 @@
+export class User{
+    public id: number
+    public nome: string
+    public usuraio: string
+    public senha: string
+}
