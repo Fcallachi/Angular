0636e5b81b74dc2e716ebb7a10174d8f92787ce0
unknown <fernandocallachi@gmail.com>
2020-11-13 17:36:23 -0300

    implementação do put e do delete em postagem e em temas

--- Blog_Pessoal/blogPessoal/src/app/app-routing.module.ts
+++ Blog_Pessoal/blogPessoal/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CadastroComponent } from './cadastro/cadastro.component';
+import { DeletePostagemComponent } from './delete-postagem/delete-postagem.component';
+import { DeleteTemaComponent } from './delete-tema/delete-tema.component';
 import { FeedComponent } from './feed/feed.component';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { PostTemaComponent } from './post-tema/post-tema.component';
+import { PutPostagemComponent } from './put-postagem/put-postagem.component';
+import { PutTemaComponent } from './put-tema/put-tema.component';
 import { TermoComponent } from './termo/termo.component';
 
 
@@ -15,7 +19,11 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'cadastro', component: CadastroComponent},
   { path: 'cadastro-tema', component: PostTemaComponent},
-  { path: 'termo',component: TermoComponent}
+  { path: 'termo',component: TermoComponent},
+  { path: 'editar-post/:id',component:PutPostagemComponent},
+  { path: 'delete-post/:id',component:DeletePostagemComponent},
+  { path: 'edita-tema/:id',component:PutTemaComponent},
+  { path: 'delete-tema/:id',component:DeleteTemaComponent}
 
 ];
 
--- Blog_Pessoal/blogPessoal/src/app/service/tema.service.ts
+++ Blog_Pessoal/blogPessoal/src/app/service/tema.service.ts
@@ -23,6 +23,11 @@ export class TemaService {
 
   postTema(tema: Tema) {
     return this.http.post('http://localhost:8081/tema', tema, this.token)
-
+  }
+  putTema(tema:Tema){
+    return this.http.put('http://localhost:8081/tema',tema,this.token)
+  }
+  deleteTema(id:number){
+    return this.http.delete(`http://localhost:8081/tema/${id}`, this.token)
   }
 }
